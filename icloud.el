5123b8a0cbc1119c646e765d5ed0daf4e686d921
andyjda <bobodeangelis@gmail.com>
2022-12-31 15:35:45 -0500

    documentation and logging

--- icloud.el
+++ icloud.el
@@ -22,86 +22,106 @@
 
 ;; This is still a sketch, but the main idea is to:
 ;; 1) provide a way to seamlessly check if a file is not present locally,
-;; and if so, download it (`icloud-find-file')
+;; and if so, download it (`icloud-get-file')
 ;; 2) provide a way to seamlessly open the iCloud document rather than the
-;; ".icloud" local copy (`icloud-navigation-mode')
-;; 3) provide a way to download multiple files (`icloud-download-in-dir')
-;; 4) provide utilities to do these things asynchronously when needed
+;; ".icloud" local copy (`icloud-navigation-mode' and `icloud-open')
+;; 3) provide a way to download multiple files (`icloud-download-files' and
+;; `icloud-download-in-directory')
 
 ;;; Code:
+
+;;;; core utilities
 (defgroup icloud nil
   "Utilities for iCloud files."
   :group 'comm)
 
 ;; TODO: maybe there should also be a customizable multiplier that is tied to file-size
-;; eg: every megabite, add a second
+;; eg: every x megabites, add a second to the wait time
 (defcustom icloud-max-wait 3
-  "Integer denoting how many seconds `icloud-download' should wait.
-If the file still isnt' downloaded after this amount of seconds,
-error out."
+  "Integer denoting how many seconds `icloud-check-on-progress' should wait."
   :type 'integer
+  :group 'icloud)
+
+(defcustom icloud-default-open-function #'find-file
+  "Function to open files within `icloud-open'."
+  :type 'function
   :group 'icloud)
 
 (defface icloud-message
   '((t :inherit minibuffer-prompt))
-  "Face used to display message about iCloud"
+  "Face used to display messages about iCloud."
   :group 'icloud)
-
-(defun icloud-propertize-message (str)
-  (propertize str 'face 'icloud-message))
 
 (defface icloud-error
   '((t :inherit error))
-  "Face used to display error messages about iCloud"
+  "Face used to display error-messages about iCloud."
   :group 'icloud)
+
+(defun icloud-propertize-message (str)
+  "Apply `propertize' to STR with the face `icloud-message'."
+  (propertize str 'face 'icloud-message))
 
 (defun icloud-shell-command (file)
   "Build the shell command needed to download FILE."
   (concat
+   ;; TODO: maybe this should be a customizable variable
+   ;; although it doesn't seem like there are many other options:
+   ;; 1) 'brctl' is the command needed to download
+   ;; 2) it doesn't take many parameters: just the one filename
    "brctl download \"" file "\""))
 
-(defun icloud-download (file)
+(defun icloud-start-download (file)
+  "Start downloading FILE from iCloud, using `icloud-shell-command'."
+  ;; TODO: it's possible that additional steps might be needed
+  ;; such as `'brctl evict' (see https://apple.stackexchange.com/questions/328329/is-there-a-way-via-the-command-line-to-cause-icloud-files-to-download)
+  ;; although this step is automatic in my system
   (shell-command (icloud-shell-command file))
   (message "%s: %s"
            (icloud-propertize-message "Downloading")
            file))
-
+;; TODO: it might be good to use'brctl log' instead here
+;; (see https://apple.stackexchange.com/questions/328329/is-there-a-way-via-the-command-line-to-cause-icloud-files-to-download)
+;; although I'll have to get familiar with its output
 (defun icloud-check-on-progress (file)
+  "Check if FILE exists.  Wait for no more than `icloud-max-wait' seconds."
   (let ((start-time (current-time)))
     (while (and
             (not (file-exists-p file))
+            ;; get the seconds passed since `start-time':
             (> icloud-max-wait (nth 1 (time-subtract nil start-time))))
       (sleep-for 0.005)))
   (file-exists-p file)) ; return nil when download doesn't succeed
 
 (defun icloud-log (to-message &rest string)
-  "Log STRING to a buffer."
-  ;; (message "string: %s" string)
-  ;; (message "(car string): %s" (car string))
+  "Log STRING to a *icloud-progress-log* buffer.
+If TO-MESSAGE is non-nil, also `message' STRING"
   (let ((buf (get-buffer-create "*icloud-progress-log*"))
         (string (apply #'format string)))
     (when to-message
       (message string))
     (with-current-buffer buf
-      (end-of-buffer)
+      (goto-char (point-max))
       (insert string "\n"))))
 
 (defun icloud-report-on-progress (file &optional error to-message)
-  "Check on progress and log about it in *icloud-progrss-log*.
-If ERROR is non-nil, create an error when the file isn't downloaded.
-If TO-MESSAGE is non-nil, also send progress as `message'"
+  "Check on FILE's download progress and log it in *icloud-progress-log*.
+Use `icloud-check-on-progress' to wait for the download to succeed.
+If FILE doesn't exist after waiting for `icloud-max-wait', return nil.
+If ERROR is non-nil, throw an error when the file isn't downloaded.
+If TO-MESSAGE is non-nil, also use `message' to display progress"
   (let ((success (icloud-check-on-progress file)))
     (if success
         (progn
           (icloud-log
            to-message
-           "%s: %s"
-           (icloud-propertize-message "Download succeeded")
+           "%s %s"
+           (icloud-propertize-message "Download succeeded:")
            file)
           file) ;; return the file name
       (let ((failed-message
-             (format "%s: %s"
-                     (icloud-propertize-message "Failed to download this file")
+             (format "%s %s"
+                     (propertize "Failed to download this file:"
+                                 'face 'icloud-error)
                      file)))
         (icloud-log to-message failed-message)
         (when error
@@ -109,12 +129,15 @@ If TO-MESSAGE is non-nil, also send progress as `message'"
         nil))))
 
 (defun icloud-local-to-download (path)
+  "Convert PATH to its name in the cloud.
+If PATH is not a cloud file, return nil.
+If PATH is a non-downloaded local copy of an iCloud file
+\(eg \"my_directory/.my_file.txt.icloud\"),
+return its name stripped of the starting \".\" and the trailing \".icloud\"
+\(eg \"my_directory/my-file.txt\")"
   (let* ((path (expand-file-name path))
          (filename (file-name-nondirectory path))
          (length (length filename)))
-    ;; (message "path: %s" path)
-    ;; (message "filename: %s" filename)
-    ;; (message "length: %s" length)
     (if (and (> length 7)
              (equal (substring filename -7 length) ".icloud")
              (eq (aref filename 0) ?.))
@@ -124,66 +147,95 @@ If TO-MESSAGE is non-nil, also send progress as `message'"
       nil)))
 
 (defun icloud-get-file (file &optional error)
+  "Get FILE from iCloud, wait on its progress, return its name.
+FILE should be the name of the file in the cloud.
+If FILE doesn't exist yet, try downloading it.
+If FILE doesn't exist after waiting for `icloud-max-wait', return nil.
+If ERROR is non-nil, throw an error when download fails"
   (if (file-exists-p file)
-      file
+      (progn
+        (message "%s %s"
+                 (icloud-propertize-message
+                  "This file already exists:")
+                 file)
+        file)
     (progn
-      (icloud-download file)
+      (icloud-start-download file)
       (icloud-report-on-progress file error 'to-message))))
 
 (defun icloud-get-file-if-cloud (file &optional error)
+  "Get FILE from iCloud, wait on its progress, return name of local copy.
+When file is a non-downloaded local copy of an iCloud file
+\(eg \"my_dir/.my_file.txt.icloud\"),
+convert it to its name in the cloud, and download it with `icloud-get-file'.
+When FILE is not in this format, return its name (the argument FILE as given).
+If ERROR is non-nil, throw an error when download fails"
   (let ((cloud-file (icloud-local-to-download file)))
     (if cloud-file
         (icloud-get-file cloud-file error)
       (progn
-        (message "%s: %s"
-                 (icloud-propertize-string
-                  "icloud: this file is already a local copy")
+        (message "%s %s"
+                 (icloud-propertize-message
+                  "This file is not an \"*.icloud\" file:")
                  file)
         file))))
 
-;; TODO: need better names for these functions (`icloud-interactive-download'
-;; and `icloud-download')
+(defun icloud-find-file (file)
+  "Open FILE, downloading it if needed.
+This is intended for programmatic use: it expects a file-name
+without trailing \".icloud\", so your program won't have to worry about
+the file's name changing when the file is in the cloud.
+For interactive use, see
+`icloud-navigation-mode' and `icloud-open'."
+  (find-file (icloud-get-file file)))
+
+;;;; interactive interface for single-file downloads
+
 ;;;###autoload
-(defun icloud-download-this (file)
+(defun icloud-download (file)
+  "Download FILE from iCloud, wait on its progress, return name of local copy.
+This function is to be used interactively.
+For use in a program, see `icloud-start-download' or `icloud-get-file-if-cloud'"
   (interactive
    (list (read-file-name "icloud download: ")))
   (icloud-get-file-if-cloud file 'error))
 
 ;;;###autoload
-(defun icloud-async-download-this (file)
+(defun icloud-async-download (file)
+  "Download FILE from iCloud. Report on its progress in an async thread."
   (interactive
    (list (read-file-name "icloud async download: ")))
   (let ((file (icloud-local-to-download file)))
-    (icloud-download file)
+    (icloud-start-download file)
     (make-thread
-     (lambda () (icloud-report-on-progress file nil t)))))
-
-(defcustom icloud-default-open-function #'find-file
-  "Function to open files within `icloud-open'"
-  :type 'function
-  :group 'icloud)
+     (lambda ()
+       (icloud-log
+        nil
+        "%s %s."
+        (icloud-propertize-message "-----------\nDownloading file:") file)
+       (icloud-report-on-progress file nil t)))))
 
 ;;;###autoload
 (defun icloud-open (&optional function)
+  "Temporarily activate `icloud-navigation-mode' and run FUNCTION to open files.
+This is to leverage the completion capabilities and configuration of FUNCTION,
+while `icloud-navigation-mode' ensures that the file is downloaded if needed.
+When FUNCTION is nil, we use `icloud-default-open-function'.
+FUNCTION should be an interactive function:
+for opening cloud files within a program, see `icloud-find-file'"
   (interactive)
   (unwind-protect
       (progn
         (icloud-navigation-mode 1)
         (call-interactively (or function icloud-default-open-function)))
-    (icloud-navigation-mode 'toggle))) ;; TODO: should this be toggle or -1?
+    (icloud-navigation-mode -1)))
 
-;; TODO: maybe try a better `icloud-find-file' for interactive single download
-(defun icloud-find-file-internal (file)
-  "This is intended for programmatic use: it expects a FILENAME
-without trailing \".icloud\", so your program won't have to worry about
-the FILENAME changing when the file is in the cloud.
-For interactive use, see `icloud-navigation-mode'."
-  (find-file (icloud-get-file file)))
+;;;; always download file when needed
 
-(defun adv/icloud-download-if-cloud (filename)
-  "Function used as advice to `insert-file-contents'.
+(defun adv/icloud-get-file-if-cloud (filename)
+  "Function used by `icloud-navigation-mode' to advice `insert-file-contents'.
 It ensures that, if FILENAME is in the cloud, we download it before opening it.
-This is done by calling `icloud-download-if-cloud' while filtering arguments
+This is done by calling `icloud-get-file-if-cloud' while filtering arguments
 to `insert-file-contents'"
   (let ((downloaded-file
          (icloud-get-file-if-cloud (car filename) 'error)))
@@ -192,30 +244,41 @@ to `insert-file-contents'"
     ;; TODO: fix this; maybe the download should happen earlier?
     ;; need to look into when the buffer-name is created
     (rename-buffer (file-name-nondirectory downloaded-file) 'unique)
+    ;; `insert-file-contents' expects arguments in a list
     (list downloaded-file (cdr filename))))
 
 ;;;###autoload
 (define-minor-mode icloud-navigation-mode
-  "Minor mode to download iCloud files when we try to open \".icloud\" files.
-this uses `icloud-download-if-cloud', which converts any file with
-trailing \".icloud\" into its expected format, and tries to download it."
+  "Minor mode to download iCloud files when opening non-downloaded local copies.
+This uses `icloud-get-file-if-cloud', which converts any file with
+trailing \".icloud\" into its expected format, and tries to download it.
+This advises `insert-file-contents',
+so for every file we open (interactively or not),
+it will check if it needs to be downloaded, and if so, attempt a download.
+For one-time interactive use, see `icloud-open'.
+For non-interactive use, see `icloud-find-file'"
   :global t
   :lighter " iCloud"
   :group 'comm
   (if icloud-navigation-mode
       (advice-add 'insert-file-contents :filter-args
-                  #'adv/icloud-download-if-cloud)
-    (advice-remove 'insert-file-contents #'adv/icloud-download-if-cloud)))
+                  #'adv/icloud-get-file-if-cloud)
+    (advice-remove 'insert-file-contents #'adv/icloud-get-file-if-cloud)))
 
-;; TODO: there must a better approach to interactive/default arguments?
+;;;; download multiple files
+
 ;;;###autoload
 (defun icloud-download-in-directory (&optional directory regexp recursively report)
   "Find all iCloud files matching REGEXP in DIRECTORY, and download them.
+DIRECTORY defaults to `default-directory'; REGEXP defaults to an empty string.
 If RECURSIVELY is non-nil, also find files within directories inside DIRECTORY.
-If REPORT is non-nil, start an async thread to check on download progress.
-To check they are in the cloud, REGEXP is altered to include a beginning
+If REPORT is non-nil, start an async thread to check on download progress,
+using the buffer *icloud-progress-log*.
+To check if files are in the cloud, REGEXP is altered to include a beginning
 \"\\..*\" and a trailing \".*\\.icloud\""
   (interactive)
+  ;; set the appropriate values
+  ;; TODO: is there a better approach to interactive/default arguments?
   (let* ((directory
           (or directory
               (when (called-interactively-p 'any)
@@ -241,9 +304,10 @@ To check they are in the cloud, REGEXP is altered to include a beginning
                 (length
                  (read-string
                   "Report on progress? (empty means NO, else YES) "))))))
+    ;; log what we will download
     (icloud-log
-     'to-message
-     "%s %s,\n%s %s, \n%s %s, %s %s."
+     nil
+     "%s %s\n%s %s \n%s %s %s %s."
      (icloud-propertize-message "-----------\nDownloading in directory:")
      directory
      (icloud-propertize-message "for regexp:") regexp
@@ -254,7 +318,7 @@ To check they are in the cloud, REGEXP is altered to include a beginning
                                (directory-files-recursively directory regexp)
                              (directory-files directory 'full regexp))
                            report)))
-
+;;;###autoload
 (defun icloud-download-files (files &optional report)
   "Download FILES from the cloud, if they are non-downloaded local copies.
 If REPORT is non-nil, start an async thread to check on download progress,
@@ -264,13 +328,22 @@ using the buffer *icloud-progress-log*."
                 files)))
     (shell-command
      (mapconcat #'icloud-shell-command files " && "))
-    (if report
-        (make-thread
-         (lambda ()
-           (mapc #'icloud-report-on-progress files)
-           (icloud-log 'to-message
-                       (icloud-propertize-message
-                        "iCloud report finished for all documents\n")))))))
+    (when report
+      (let ((thread
+             (make-thread
+              (lambda ()
+                (mapc #'icloud-report-on-progress files)
+                (icloud-log 'to-message
+                            "%s %s\n"
+                            (icloud-propertize-message
+                             "iCloud report finished for all documents at thread:")
+                            (current-thread))))))
+        (icloud-log 'to-message "%s %s"
+                    (icloud-propertize-message "Checking on progress at thread:")
+                    thread)))
+    (icloud-log 'to-message "%s %s files"
+                (icloud-propertize-message "Downloading")
+                (length files))))
 
 (provide 'icloud)
 ;;; icloud.el ends here
